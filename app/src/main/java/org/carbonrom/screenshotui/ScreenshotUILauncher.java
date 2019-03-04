1f404c363a98881492c2352af0a088967d8e5ddb
Joe Maples <joe@frap129.org>
2019-03-04 08:31:48 -0500

    Launcher: Request overlay permission
    Avoid needless crashes
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- app/src/main/java/org/carbonrom/screenshotui/ScreenshotUILauncher.java
+++ app/src/main/java/org/carbonrom/screenshotui/ScreenshotUILauncher.java
@@ -2,7 +2,9 @@ package org.carbonrom.screenshotui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 
 
 // REMOVE THIS CLASS LATER
@@ -11,6 +13,10 @@ public class ScreenshotUILauncher extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 0);
+        }
         Intent svc = new Intent(this, ScreenshotUIService.class);
         startService(svc);
         finish();
